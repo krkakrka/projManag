171ae99a3fec8f1c8a29f798d60ce6a57c6675e4
Karolis Lesevicius <karolis.lesevicius@gasefi.com>
2017-04-27 23:46:02 +0300

    Enable name editing

--- projectManager.js
+++ projectManager.js
@@ -1,12 +1,20 @@
-function ProjectManager(){
+// todo possible coupling of DOM and data
+function ProjectManager(projectSettings){
   this.projectData = {};
+  // input, will modify value
+  this.nameElement = document.getElementById(projectSettings.nameElementId);
+  // anything, will modify innerHTML
+  this.dataElement = document.getElementById(projectSettings.dataElementId);
 }
-ProjectManager.prototype.loadProjectData = function(data){
-  this.projectData = data;
+ProjectManager.prototype.renderData = function(){
+  this.dataElement.innerHTML = JSON.stringify(this.projectData);
 }
-ProjectManager.prototype.renderData = function(elementId){
-  var projectDataEl = document.getElementById(elementId);
-  projectDataEl.innerHTML = JSON.stringify(this.projectData);
+ProjectManager.prototype.renderName = function(){
+  this.nameElement.value = this.projectData.name;
+}
+ProjectManager.prototype.renderAll = function(){
+  this.renderData();
+  this.renderName();
 }
 ProjectManager.prototype.downloadProjectData = function(){
   var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.projectData));
@@ -17,6 +25,7 @@ ProjectManager.prototype.downloadProjectData = function(){
   a.click();
 }
 // todo return promise or use callback? Or some sort of event? So that something else could know that data has been updated.
+// todo possible bad separation of concerns. ProjectManager doesnt need to know how to handle files
 ProjectManager.prototype.updateProjectDataWithFile = function(file){
   // hack to reach projManag instance
   var self = this;
@@ -24,6 +33,7 @@ ProjectManager.prototype.updateProjectDataWithFile = function(file){
   // async callback
   reader.onload = function(event){
     self.projectData = JSON.parse(event.target.result);
+    self.renderAll();
   };
   reader.readAsText(file);
 }
